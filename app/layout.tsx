0e4cbb4dd9f69bdfb739045d781d7863307976d0
Red <100178989+rebekahdhey@users.noreply.github.com>
2024-10-21 09:18:06 -0700

    Add files via upload

---
+++ app/layout.tsx
@@ -0,0 +1,41 @@
+import type { Metadata } from "next";
+import localFont from "next/font/local";
+import "./globals.css";
+import NavBar from "@/components/NavBar";
+import Footer from "@/components/Footer";
+
+
+const geistSans = localFont({
+  src: "./fonts/GeistVF.woff",
+  variable: "--font-geist-sans",
+  weight: "100 900",
+});
+const geistMono = localFont({
+  src: "./fonts/GeistMonoVF.woff",
+  variable: "--font-geist-mono",
+  weight: "100 900",
+});
+
+export const metadata: Metadata = {
+  title: "Travel",
+  description: "Travel App for Camping",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body>
+        <NavBar />
+        <main className="relative overflow-hidden">
+          {children}
+        </main>
+        
+        <Footer />
+      </body>
+    </html>
+  );
+}
---
+++ next.config.mjs
@@ -0,0 +1,11 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    images: {
+      // Next.js automatically optimizes images in the 'public' directory
+      // You don't need to add anything here for local images
+      unoptimized: false, // Optional: set to true if you want to disable optimization
+    },
+  };
+  
+  export default nextConfig;
+  
